4c88774e1fdd64ae243bbae8454cd026b7a37b42
jlsuarezdiaz <jlsuarezdiaz@correo.ugr.es>
2017-02-05 21:37:14 +0100

    Admin

--- sql/packages/admin.sql
+++ sql/packages/admin.sql
@@ -1,8 +1,8 @@
 CREATE OR REPLACE PACKAGE BODY PKG_ADMIN AS
 
-FUNCTION estaEnComunidad(nombre VARCHAR2) return VARCHAR2 IS nc VARCHAR2(20);
+FUNCTION estaEnComunidad(nombre VARCHAR2, comunidad VARCHAR2) return INTEGER IS nc INTEGER;
 BEGIN
-  select nombre_comunidad into nc from Pertenece where nombre_usu=nombre;
+  select count(*) into nc from Pertenece where nombre_usu=nombre and comunidad = nombre_comunidad;
   return(nc);
 END;
 
@@ -14,10 +14,19 @@ END;
 
 PROCEDURE cambiar_privilegios_admin(usu VARCHAR2, comunidad VARCHAR2) AS
 BEGIN
-  IF (esAdmin(usu,comunidad) = 0) THEN
-    update Pertenece set administrador = 1 where usu=nombre_usu and nombre_comunidad=comunidad;
-  ELSE
-    update Pertenece set administrador = 0 where usu=nombre_usu and nombre_comunidad=comunidad;
+  IF (estaEnComunidad(usu,comunidad) = 1) THEN
+    IF (esAdmin(usu,comunidad) = 0) THEN
+      update Pertenece set administrador = 1 where usu=nombre_usu and nombre_comunidad=comunidad;
+    ELSE
+      update Pertenece set administrador = 0 where usu=nombre_usu and nombre_comunidad=comunidad;
+    END IF;
+  END IF;
+END;
+
+PROCEDURE otorgarCreditos(usu VARCHAR2, premio INTEGER, comunidad VARCHAR2) AS
+BEGIN
+  IF (estaEnComunidad(usu,comunidad) = 1) THEN
+    update Pertenece  set creditos = creditos + premio where usu=nombre_usu and comunidad = nombre_comunidad;
   END IF;
 END;
 
--- sql/packages/admin_h.sql
+++ sql/packages/admin_h.sql
@@ -3,6 +3,6 @@ CREATE OR REPLACE PACKAGE PKG_ADMIN AS
 FUNCTION estaEnComunidad(nombre VARCHAR2, comunidad VARCHAR2) return INTEGER;
 FUNCTION esAdmin(nombre VARCHAR2, comunidad VARCHAR2) return INTEGER;
 PROCEDURE cambiar_privilegios_admin(usu VARCHAR2, comunidad VARCHAR2);
-PROCEDURE otorgarCreditos(usu VARCHAR2, creditos INTEGER);
+PROCEDURE otorgarCreditos(usu VARCHAR2, premio INTEGER, comunidad VARCHAR2);
 
 END PKG_ADMIN;
