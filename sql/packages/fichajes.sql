748e553dc844db1ef0fd2916eb16a2241674e9e6
rbnuria <rbnuria6@gmail.com>
2017-02-05 20:28:52 +0100

    No funciona el merge :(

--- sql/packages/fichajes.sql
+++ sql/packages/fichajes.sql
@@ -1,12 +1,18 @@
 CREATE OR REPLACE PACKAGE BODY PKG_FICHAJES AS
 
-/*PROCEDURE Pujar(nomb_usuario VARCHAR2, nomb_comunidad VARCHAR2,id_jugador INTEGER, cantidad REAL) AS
+FUNCTION precio_minimo(codigo_jugador INT, nombre_comunidad VARCHAR2) return INTEGER IS precio INTEGER;
+BEGIN
+  select precio_min into precio from ApareceEn where codigo_jugador=codigo_jugador and nombre_comunidad=nombre_comunidad;
+  return(precio);
+END;
+
+PROCEDURE Pujar(nomb_usuario VARCHAR2, nomb_comunidad VARCHAR2,id_jugador INTEGER, cantidad INT) AS
 BEGIN 
  
- IF (select precio_min from ApareceEn where codigo_jugador=id_jugador and NOMBRE_COMUNIDAD=nomb_comunidad) <= cantidad THEN
-  INSERT into RealizarOferta(nombre_usuario,nombre_comunidad,codigo_jugador,precio) values (nomb_usuario, nomb_comunidad, id_jugador, cantidad);
-  END IF;
-END Pujar;*/
+ IF (precio_minimo(id_jugador, nomb_comunidad) <= cantidad) THEN
+   INSERT into RealizarOferta(nombre_usuario,nombre_comunidad,codigo_jugador,precio, estado) values (nomb_usuario, nomb_comunidad, id_jugador, cantidad, 0);
+ END IF;
+END Pujar;
 
 PROCEDURE ofrecer_sistema(nomb_comunidad VARCHAR2,id_jugador INTEGER) AS
 BEGIN 
@@ -14,9 +20,8 @@ BEGIN
   INSERT into ApareceEn(NOMBRE_VENDEDOR, nombre_comunidad,codigo_jugador,precio_min) values ('COMPUTER',nomb_comunidad, id_jugador, (select precio from Jugadores where cod=id_jugador));
 END ofrecer_sistema;
 
-PROCEDURE ofrecer_jugador(nomb_usuario VARCHAR2, nomb_comunidad VARCHAR2, id_jugador INTEGER, precio REAL) AS
-BEGIN 
-  
+PROCEDURE ofrecer_jugador(nomb_usuario VARCHAR2, nomb_comunidad VARCHAR2, id_jugador INTEGER, precio INT) AS
+BEGIN  
   INSERT into ApareceEn(NOMBRE_VENDEDOR, nombre_comunidad,codigo_jugador,precio_min) values (nomb_usuario,nomb_comunidad, id_jugador, precio);
 END ofrecer_jugador;
 
--- sql/packages/fichajes_h.sql
+++ sql/packages/fichajes_h.sql
@@ -1,9 +1,11 @@
 CREATE OR REPLACE PACKAGE PKG_FICHAJES AS
 
-/*PROCEDURE Pujar(nomb_usuario VARCHAR2, nomb_comunidad VARCHAR2,id_jugador INTEGER, cantidad REAL);*/
+FUNCTION precio_minimo(codigo_jugador INT, nombre_comunidad VARCHAR2) return INTEGER;
+
+PROCEDURE Pujar(nomb_usuario VARCHAR2, nomb_comunidad VARCHAR2,id_jugador INTEGER, cantidad INT);
 
 PROCEDURE ofrecer_sistema(nomb_comunidad VARCHAR2, id_jugador INTEGER);
 
-PROCEDURE ofrecer_jugador(nomb_usuario VARCHAR2, nomb_comunidad VARCHAR2, id_jugador INTEGER, precio REAL);
+PROCEDURE ofrecer_jugador(nomb_usuario VARCHAR2, nomb_comunidad VARCHAR2, id_jugador INTEGER, precio INT);
 
 END PKG_FICHAJES;
