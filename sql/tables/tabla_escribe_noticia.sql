ad511daff602f4e54d5ca9873a804a22490c4edd
Javier Poyatos <jpoyatosamador@correo.ugr.es>
2017-02-06 10:39:05 +0100

    escribe noticia

--- sql/tables/tabla_escribe_noticia.sql
+++ sql/tables/tabla_escribe_noticia.sql
@@ -5,8 +5,8 @@ create table EscribeNoticia(
   nombre_comunidad varchar(20) NOT NULL,
   codigo_noticia int NOT NULL,
   fecha date,
-  PRIMARY KEY (nombre_usu, nombre_comunidad, codigo_noticia),
-  FOREIGN KEY (nombre_usu) REFERENCES Usuario(nombre_usu),
+  PRIMARY KEY (nombre_us, nombre_comunidad, codigo_noticia),
+  FOREIGN KEY (nombre_us) REFERENCES Usuario(nom_usu),
   FOREIGN KEY (nombre_comunidad, codigo_noticia) REFERENCES TablonAnuncios(nombre_comunidad,codigo_noticia)
 );
 
