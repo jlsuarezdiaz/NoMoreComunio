128b300b425feb4f8ca4e6549e5a90ea46e508c9
Javier Poyatos <jpoyatosamador@correo.ugr.es>
2017-02-07 19:19:04 +0100

    admin + global

--- sql/packages/global.sql
+++ sql/packages/global.sql
@@ -1,5 +1,30 @@
 CREATE OR REPLACE PACKAGE BODY PKG_GLOBAL AS
 
+FUNCTION obtenerCodigoPrimero return INTEGER IS codigo INTEGER;
+BEGIN
+  select cod into codigo from (select cod from Jugadores order by dbms_random.value) where rownum=1;
+  return(codigo);
+END;
+
+PROCEDURE modificarMercado(numjug INTEGER, comunidad VARCHAR2) AS
+  contador INTEGER;
+  codigo INTEGER;
+  precioj INTEGER;
+  estaen INTEGER;
+BEGIN
+  delete from ApareceEn where nombre_comunidad=comunidad and nombre_vendedor='COMPUTER';
+    
+  FOR contador IN 1..numjug LOOP
+    codigo:=obtenerCodigoPrimero;
+      
+    select count(*) into estaen from ApareceEn where codigo_jugador=codigo and nombre_comunidad=comunidad;
+      
+    IF (estaen = 0) THEN
+      select precio into precioj from Jugadores where cod=codigo; 
+      insert into ApareceEn values ('COMPUTER',comunidad,codigo,precioj);
+    END IF;
+  END LOOP;
+END;
 
 FUNCTION calcMax(comunidad VARCHAR2) return INTEGER IS maximo INTEGER;
 contador INT;
@@ -14,18 +39,32 @@ BEGIN
   return(maximo);
 END;
 
-FUNCTION totalJugadores return INTEGER IS total INTEGER;
+FUNCTION estaEnComunidad(nombre VARCHAR2, comunidad VARCHAR2) return INTEGER IS nc INTEGER;
 BEGIN
-  SELECT COUNT (*) INTO total FROM Jugadores;
+  select count(*) into nc from Pertenece where nombre_usu=nombre and comunidad = nombre_comunidad;
+  return(nc);
+END;
+
+FUNCTION totalJugadores return INTEGER IS total INTEGER;
+contador INT;
+BEGIN
+  SELECT COUNT (*) INTO contador FROM Jugadores;
+  
+  IF contador > 0 THEN
+    select max(cod) into total from Jugadores;
+  ELSE
+    return(0);
+  END IF;
   return (total);
 END;
 
 PROCEDURE escribeNoticia(usuario VARCHAR2, comunidad VARCHAR2, noticia VARCHAR2) AS
 maximo INTEGER;
 BEGIN
-  insert into TablonAnuncios(nombre_comunidad,codigo_noticia,noticia) values (comunidad,calcMax(comunidad)+1,noticia);
-  insert into EscribeNoticia(nombre_us,nombre_comunidad,codigo_noticia,fecha) values (usuario,comunidad,calcMax(comunidad),SYSDATE);
-
+  IF (estaEnComunidad(usuario,comunidad) =  1) THEN
+    insert into TablonAnuncios(nombre_comunidad,codigo_noticia,noticia) values (comunidad,calcMax(comunidad)+1,noticia);
+    insert into EscribeNoticia(nombre_us,nombre_comunidad,codigo_noticia,fecha) values (usuario,comunidad,calcMax(comunidad),SYSDATE);
+  END IF;
 END;
 
 PROCEDURE registrarJugador(nombre VARCHAR2, equipo VARCHAR2, pos VARCHAR2, precio INTEGER) AS
@@ -46,6 +85,19 @@ BEGIN
   OPEN devolver FOR
   SELECT nombre_comunidad FROM PERTENECE where nombre_usu = usuario;
   
+  /*
+  LOOP
+    FETCH devolver INTO nombre_comunidad;
+    dbms_output.put_line(nombre_comunidad);
+    EXIT WHEN (devolver%NOTFOUND);
+  END LOOP;
+  
+    WHILE(devolver%FOUND) LOOP
+      FETCH devolver INTO nombre_comunidad;
+      dbms_output.put_line(nombre_comunidad);
+    END LOOP;
+*/  
+
 END;
 
 
@@ -65,13 +117,13 @@ BEGIN
   (select * from EscribeNoticia, TABLONANUNCIOS 
   where escribenoticia.nombre_comunidad = tablonanuncios.nombre_comunidad 
   and escribenoticia.codigo_noticia = tablonanuncios.codigo_noticia and escribenoticia.nombre_comunidad = comunidad);
- /* 
+  
   LOOP
     FETCH devolver INTO noticia, fecha, usuario;
     EXIT WHEN (devolver%NOTFOUND);
 
   END LOOP;
-  CLOSE devolver;*/
+  CLOSE devolver;
   
 END;
 
--- sql/packages/global_h.sql
+++ sql/packages/global_h.sql
@@ -1,6 +1,9 @@
 CREATE OR REPLACE PACKAGE PKG_GLOBAL AS
 
+FUNCTION obtenerCodigoPrimero return INTEGER;
+PROCEDURE modificarMercado(numjug INTEGER, comunidad VARCHAR2);
 FUNCTION calcMax(comunidad VARCHAR2) return INTEGER;
+FUNCTION estaEnComunidad(nombre VARCHAR2, comunidad VARCHAR2) return INTEGER;
 FUNCTION totalJugadores return INTEGER;
 PROCEDURE escribeNoticia(usuario VARCHAR2, comunidad VARCHAR2, noticia VARCHAR2);
 PROCEDURE registrarJugador(nombre VARCHAR2, equipo VARCHAR2, pos VARCHAR2, precio INTEGER);
