375c9962f01ec8e543398384043b0e0dd5f65b92
Javier Poyatos <jpoyatosamador@correo.ugr.es>
2017-02-08 01:56:19 +0100

    funciones conexion java-sql

--- src/Model/DBFunctions.java
+++ src/Model/DBFunctions.java
@@ -148,6 +148,128 @@ public class DBFunctions {
         return tablon;
     }
     
+    public static void ponerJugadorEnOnce(String usu, String com, Integer cod, int ronda) throws SQLException{
+        Connection con = DBConnect();
+        
+        //Consulta que se va a realizar (los argumentos se escriben como ?, se especifican después). 
+        String jobquery = "begin pkg_fichajes.ponerJugadorEnOnce(?,?,?,?); end;";
+        
+        //Preparamos la llamada.
+        CallableStatement callStmt = con.prepareCall(jobquery);
+        
+        //Parámetros de entrada
+        callStmt.setString(1,usu);
+        callStmt.setString(2,com);
+        callStmt.setInt(3,cod);
+        callStmt.setInt(4,ronda);
+        
+        // Ejecutamos el comando.
+        callStmt.execute();   
+    }
+    
+    public static void borrarAlineacion(String usu, String com, int ronda) throws SQLException{
+        Connection con = DBConnect();
+        
+         //Consulta que se va a realizar (los argumentos se escriben como ?, se especifican después). 
+        String jobquery = "begin pkg_fichajes.borrarAlineacion(?,?,?); end;";
+        
+        //Preparamos la llamada.
+        CallableStatement callStmt = con.prepareCall(jobquery);
+        
+        //Parámetros de entrada
+        callStmt.setString(1,usu);
+        callStmt.setString(2,com);
+        callStmt.setInt(3,ronda);
+               
+        // Ejecutamos el comando.
+        callStmt.execute();
+    }
+    
+    public static ArrayList<Player> obtenerAlineacion(String com) throws SQLException{
+        Connection con = DBConnect();
+        
+        //Consulta que se va a realizar (los argumentos se escriben como ?, se especifican después).
+        String jobquery = "begin pkg_fichajes.obtenerAlineacion(?,?); end;";
+        
+        //Preparamos la llamada.
+        CallableStatement callStmt = con.prepareCall(jobquery);
+        //Parámetros de salida
+        callStmt.registerOutParameter(2,OracleTypes.CURSOR);
+        
+        //Parámetros de entrada
+        callStmt.setString(1,com);
+        
+        //Ejecutamos comando.
+        callStmt.execute();
+        
+        ResultSet rset = (ResultSet)callStmt.getObject(2);
+        
+        ArrayList<Player> alineacion = new ArrayList();
+        
+        // nombre, equipo,pos, precio, sum(goles) as sumg, sum(asistencias) as suma, sum(t_amarillas) as sumta, sum(t_rojas) as sumtr, sum(valoracion) as sumval
+        //    1      2    3      4             5                     6                      7                      8                        9       
+        while(rset.next()){
+            String rNom = rset.getString(1);
+            String rTeam = rset.getString(2);
+            String rPos = rset.getString(3);
+            int rPMin = -1;
+            int rPrec = rset.getInt(4);
+            String rVend = null;
+            int rGol = rset.getInt(5);
+            int rAsist = rset.getInt(6);
+            int rEnc = 0;
+            int rTAma = rset.getInt(7);
+            int rTRoja = rset.getInt(8);
+            int rPts = rset.getInt(9);
+            alineacion.add(new Player(rNom,rTeam,rPos,rVend,rPMin,rPrec,rGol,rAsist,rEnc,rTAma,rTRoja,rPts));
+        }
+        
+        return alineacion;
+    }
+    
+    public static ArrayList<Player> obtenerMisJugadores(String com) throws SQLException{
+        Connection con = DBConnect();
+        
+        //Consulta que se va a realizar (los argumentos se escriben como ?, se especifican después).
+        String jobquery = "begin pkg_fichajes.obtenerMisJugadores(?,?); end;";
+        
+        //Preparamos la llamada.
+        CallableStatement callStmt = con.prepareCall(jobquery);
+        //Parámetros de salida
+        callStmt.registerOutParameter(2,OracleTypes.CURSOR);
+        
+        //Parámetros de entrada
+        callStmt.setString(1,com);
+        
+        //Ejecutamos comando.
+        callStmt.execute();
+        
+        ResultSet rset = (ResultSet)callStmt.getObject(2);
+        
+        ArrayList<Player> jugadores = new ArrayList();
+        
+        // nombre, equipo,pos, precio, sum(goles) as sumg, sum(asistencias) as suma, sum(t_amarillas) as sumta, sum(t_rojas) as sumtr, sum(valoracion) as sumval
+        //    1      2    3      4             5                     6                      7                      8                        9       
+        while(rset.next()){
+            String rNom = rset.getString(1);
+            String rTeam = rset.getString(2);
+            String rPos = rset.getString(3);
+            int rPMin = -1;
+            int rPrec = rset.getInt(4);
+            String rVend = null;
+            int rGol = rset.getInt(5);
+            int rAsist = rset.getInt(6);
+            int rEnc = 0;
+            int rTAma = rset.getInt(7);
+            int rTRoja = rset.getInt(8);
+            int rPts = rset.getInt(9);
+            jugadores.add(new Player(rNom,rTeam,rPos,rVend,rPMin,rPrec,rGol,rAsist,rEnc,rTAma,rTRoja,rPts));
+        }
+        
+        return jugadores;
+    }
+    
+    
     public static ArrayList<Player> obtener_jugadores(String com) throws SQLException {
         Connection con = DBConnect();
 
